3c1849651c1e76fce84c8e0fb86ac7ce6c5b05f3
sudhakar jha <jhasudhakar.ai@gmail.com>
2020-07-19 11:17:44 +0530

    Before Action creator

--- 6_redux/counter/src/index.js
+++ 6_redux/counter/src/index.js
@@ -6,6 +6,7 @@ import runTests from './reducers/noteReducer.test'
 
 const store = createStore(noteReducer)
 
+/*
 const App = () => {
   return(
     <div>
@@ -19,6 +20,51 @@ const App = () => {
     </div>
   )
 }
+*/
+
+//==============================================
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note =>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+//===============================================
 
 const renderApp = () => {
   console.log(store.getState());
